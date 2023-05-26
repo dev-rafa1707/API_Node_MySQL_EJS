805497c74227f9bf8063cc0d65623f23ea95e15a
dev-rafa1707 <rafa1707@gmail.com>
2023-05-26 11:01:10 -0300

    corrige acesso ao banco de dados

--- config/dbconnection.js
+++ config/dbconnection.js
@@ -2,7 +2,7 @@ const sequelize = require('sequelize');
 require('dotenv').config();
 
 
-const connection = new sequelize(process.env.DB_NAME, process.env.BD_USER, process.env.DB_PASSWORD, {
+const connection = new sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   dialect: "mysql",
   define: {
